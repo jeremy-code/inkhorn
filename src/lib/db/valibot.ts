4cdea3e403a443b5a4c3d9f350ca0d4b176a5a1d
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2024-01-25 21:15:00 -0800

    chore: update valibot type coercion

--- src/lib/db/valibot.ts
+++ src/lib/db/valibot.ts
@@ -2,19 +2,21 @@ import { createInsertSchema } from "drizzle-valibot";
 import type { WeekdayNumbers } from "luxon";
 import {
   array,
+  coerce,
   hexColor,
+  integer,
   isoTimeSecond,
   maxLength,
+  maxValue,
   minLength,
+  minValue,
+  number,
   string,
-  transform,
+  type NumberSchema,
   type Output,
 } from "valibot";
 
 import { courses } from "./schema";
-
-// type predicate to check if a number is a weekday number
-const isWeekdayNumber = (num: number): num is WeekdayNumbers => num >= 1 && num <= 7;
 
 export const insertCourseSchema = createInsertSchema(courses, {
   name: string([minLength(1), maxLength(100)]),
@@ -22,17 +24,7 @@ export const insertCourseSchema = createInsertSchema(courses, {
   endTime: string([isoTimeSecond()]),
   color: string([hexColor()]),
   weekdays: array(
-    transform(
-      // transforms string to integer
-      transform(string([minLength(1)]), (v) => parseInt(v)),
-      // asserts that the integer is a weekday number
-      (v) => {
-        if (!isWeekdayNumber(v)) {
-          throw new Error("Invalid weekday number");
-        }
-        return v;
-      }
-    ),
+    coerce(number([integer(), minValue(1), maxValue(7)]), Number) as NumberSchema<WeekdayNumbers>,
     [(minLength(1), maxLength(7))]
   ),
 });
